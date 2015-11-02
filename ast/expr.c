a22d90b3c3159e2e9ab1f00de1bbd4b63f5910fb
Alex Wu <mwumbojumbo@gmail.com>
2015-11-02 01:24:01 -0500

    Delete README from starter code; add expr_create functions

--- ast/expr.c
+++ ast/expr.c
@@ -1,16 +1,42 @@
 #include "expr.h"
 #include <stdlib.h>
 
-struct expr *expr_create(expr_t kind, struct expr *left, struct expr* right) {
+struct expr *expr_create(expr_t kind, struct expr *left, struct expr *right, struct expr *next) {
   struct expr *e = malloc(sizeof *e);
   e -> kind = kind;
   e -> left = left;
   e -> right = right;
+  e -> next = next;
   return e;
 }
 
-struct expr* expr_create_name(const char *n) {
-  struct expr *e = malloc(sizeof *e);
-  e -> kind = EXPR_IDENT;
-
+/*TODO: add symbol (needed in typechecker)*/
+struct expr *expr_create_name(const char *n) {
+  struct expr *e = expr_create(EXPR_IDENT, 0, 0, 0);
+  e -> name = n;
+  return e;
 }
+
+struct expr *expr_create_boolean_literal(int c) {
+  struct expr *e = expr_create(c?EXPR_TRUE:EXPR_FALSE, 0, 0, 0);
+  return e;
+}
+
+struct expr *expr_create_integer_literal(int c) {
+  struct expr *e = expr_create(EXPR_INTLIT, 0, 0, 0);
+  return e;
+}
+
+struct expr *expr_create_character_literal(char c) {
+  struct expr *e = expr_create(EXPR_CHARLIT, 0, 0, 0);
+  e -> char_literal = c;
+  return e;
+}
+
+struct expr *expr_create_string_literal(const char *str) {
+  struct expr *e = expr_create(EXPR_STRLIT, 0, 0, 0);
+  e -> string_literal = str;
+  return e;
+}
+
+/*TODO: make print function*/
--- ast/expr.h
+++ ast/expr.h
@@ -34,24 +34,31 @@ typedef enum {
 } expr_t;
 
 struct expr {
-	/* used by all expr types */
+	/* used by most expr types */
 	expr_t kind;
 	struct expr *left;
 	struct expr *right;
+
+  /*
+   *used by expr_lists and one off exprs (like groupings
+   *and arrs)
+   */
+  struct expr *next;
 
 	/* used by leaf expr types */
 	const char *name;
 	struct symbol *symbol;
 	int literal_value;
 	const char * string_literal;
+  char char_literal;
 };
 
-struct expr * expr_create( expr_t kind, struct expr *left, struct expr *right );
+struct expr * expr_create( expr_t kind, struct expr *left, struct expr *right, struct expr *next );
 
 struct expr * expr_create_name( const char *n );
 struct expr * expr_create_boolean_literal( int c );
 struct expr * expr_create_integer_literal( int c );
-struct expr * expr_create_character_literal( int c );
+struct expr * expr_create_character_literal( char c );
 struct expr * expr_create_string_literal( const char *str );
 
 void expr_print( struct expr *e );
