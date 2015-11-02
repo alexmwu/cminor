ba49808e7802e9559747bea37dae84a834129549
Alex Wu <mwumbojumbo@gmail.com>
2015-11-02 11:10:25 -0500

    Add library .h

---
+++ ast/library.h
@@ -0,0 +1,10 @@
+#ifndef _LIBRARY_H
+#define _LIBRARY_H
+
+void print_integer(int x);
+void print_string(const char *s);
+void print_boolean(int b);
+void print_character(char c);
+int integer_power(int x, int y);
+
+#endif
