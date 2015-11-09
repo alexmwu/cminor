fca5b431316cb71453ca38e55464e70e55a3ffd7
Alex Wu <mwumbojumbo@gmail.com>
2015-11-09 16:15:12 -0500

    Add scope linked list to scope.h; add scope.h implementations in scope.c

--- ast/scope.c
+++ ast/scope.c
@@ -1,19 +1,52 @@
 #include "scope.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void scope_enter() {
+  if(!curr_scope) {
+    curr_scope = malloc(sizeof *curr_scope);
+    // default bucket size, hash function
+    curr_scope -> table = hash_table_create(0, 0);
+    curr_scope -> prev = 0;
+    curr_scope -> next = 0;
+    return;
+  }
 
+  struct scope_list *new_scope = malloc(sizeof *new_scope);
+  curr_scope -> next = new_scope;
+  new_scope -> prev = curr_scope;
+  new_scope -> table = hash_table_create(0, 0);
+  new_scope -> next = 0;
+  curr_scope = new_scope;
+}
+
+void remove_scopes(struct hash_table *ht) {
+  char **key;
+  struct symbol *value;
+  hash_table_firstkey(ht);
+  while(hash_table_nextkey(ht, key, (void **) &value)){
+    free(value);
+  }
 }
 
 void scope_exit() {
-
+  remove_scopes(curr_scope -> table);
+  hash_table_clear(curr_scope -> table);
+  hash_table_delete(curr_scope -> table);
+  struct scope_list *exited_scope = curr_scope;
+  curr_scope = curr_scope -> prev;
+  free(exited_scope);
 }
 
 void scope_bind(const char *name, struct symbol *s) {
-
+  int outCode = hash_table_insert(curr_scope -> table, name, s);
+  if(!outCode) {
+    fprintf(stderr, "Error in adding a key to the hash table\n");
+    exit(1);
+  }
 }
 
 struct symbol *scope_lookup(const char *name) {
-
+  return hash_table_lookup(curr_scope -> table, name);
 }
 
--- ast/scope.h
+++ ast/scope.h
@@ -2,10 +2,20 @@
 #define SCOPE_H
 
 #include "symbol.h"
+#include "hash_table.h"
+
+struct scope_list {
+  struct hash_table *table;
+  struct scope_list *next;
+  struct scope_list *prev;
+};
+
+struct scope_list *curr_scope;
 
 void scope_enter();
 void scope_exit();
 void scope_bind(const char *name, struct symbol *s);
+// returns null if symbol not found; need to handle
 struct symbol *scope_lookup(const char *name);
 
 #endif
