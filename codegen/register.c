4d44c2570527a99ab6387ed974505a2ed230ea09
Alex Wu <mwumbojumbo@gmail.com>
2015-12-01 23:43:52 -0500

    Add register allocation/freeing code; add test

--- codegen/register.c
+++ codegen/register.c
@@ -1,2 +1,24 @@
 #include "register.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+const char *register_name(int r) {
+  return register_name_string[r];
+}
+
+int register_alloc() {
+  int i;
+  for(i = 0; i < NUM_REGISTERS; i++) {
+    if(register_is_free[i]) {
+      register_is_free[i] = 0;
+      return i;
+    }
+  }
+  fprintf(stderr, "Out of scratch registers to allocate");
+  exit(1);
+}
+
+void register_free(int r) {
+  register_is_free[r] = 1;
+}
 
--- codegen/register.h
+++ codegen/register.h
@@ -1,5 +1,16 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#define NUM_REGISTERS 7
+
+static const char *register_name_string[NUM_REGISTERS] = {"rbx", "r10", "r11", "r12", "r13", "r14", "r15"}
+static int register_is_free[NUM_REGISTERS] = {1, 1, 1, 1, 1, 1, 1}
+
+const char *register_name(int r);
+// mark register as in use
+int register_alloc();
+// remove register in use
+void register_free(int r);
+
 #endif
 
